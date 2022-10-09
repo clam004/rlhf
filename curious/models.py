e4821b436b4c67addf62b47c4288b15fb66a6311
Carson Lam <carson@Carsons-MBP.attlocal.net>
2022-10-09 11:15:17 -0700

    working?

--- curious/models.py
+++ curious/models.py
@@ -23,45 +23,22 @@ class CausalLMOutputWithCrossAttentions(ModelOutput):
 
 
 class ValueHead(nn.Module):
+
     """The ValueHead class implements a head for GPT2 that returns a scalar for each output token."""
+    
     def __init__(self, config):
+
         super().__init__()
-        self.detach_head = False
-        self.summary_type = config.summary_type if hasattr(config, "summary_type") else "last"
-        if self.summary_type == "attn":
-            raise NotImplementedError
 
-        self.summary = Identity()
-        if hasattr(config, "summary_use_proj") and config.summary_use_proj:
-            if hasattr(config, "summary_proj_to_labels") and config.summary_proj_to_labels and config.num_labels > 0:
-                num_classes = config.num_labels
-            else:
-                num_classes = config.hidden_size
-            self.summary = nn.Linear(config.hidden_size, num_classes)
+        num_value_head_outputs = 1
 
-        self.activation = Identity()
-        if hasattr(config, "summary_activation") and config.summary_activation == "tanh":
-            self.activation = nn.Tanh()
+        self.summary = nn.Linear(config.hidden_size, num_value_head_outputs)
 
-        self.first_dropout = Identity()
-        if hasattr(config, "summary_first_dropout") and config.summary_first_dropout > 0:
-            self.first_dropout = nn.Dropout(config.summary_first_dropout)
+    def forward(self, hidden_states):
 
-        self.last_dropout = Identity()
-        if hasattr(config, "summary_last_dropout") and config.summary_last_dropout > 0:
-            self.last_dropout = nn.Dropout(config.summary_last_dropout)
+        output = hidden_states
             
-        self.flatten = nn.Flatten()
-
-    def forward(self, hidden_states, cls_index=None):
-        if self.detach_head:
-            output = hidden_states.detach()
-        else:
-            output = hidden_states
-        output = self.first_dropout(output)
         output = self.summary(output)
-        output = self.activation(output)
-        output = self.last_dropout(output)
 
         return output
 
